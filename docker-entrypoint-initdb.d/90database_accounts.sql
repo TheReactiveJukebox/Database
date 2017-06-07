b92a0fb5f2b75b621e79874c6b63f6ade9f1538c
Martin Ness <m.ness@martin89.de>
2017-06-08 00:50:41 +0200

    [database] full access for backend user on databse reactivejukebox

--- docker-entrypoint-initdb.d/90database_accounts.sql
+++ docker-entrypoint-initdb.d/90database_accounts.sql
@@ -1,8 +1,5 @@
 --- create user backend
 CREATE USER backend WITH NOSUPERUSER NOCREATEDB NOCREATEROLE PASSWORD 'xxx';
 
---- switch into database reactivejukebox
-\connect reactivejukebox
-
 --- give user backend access for select, update and insert operations on table users
-GRANT ALL PRIVILEGES ON users TO backend;
+GRANT ALL PRIVILEGES ON DATABASE reactivejukebox TO backend;
