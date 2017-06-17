a306b9223231931c0b3d65b3ac6ed1e235ccc53f
Martin Ness <m.ness@martin89.de>
2017-06-17 15:35:43 +0200

    [songfile] relation is not necessary

--- docker-entrypoint-initdb.d/20create_table_for_music.sql
+++ docker-entrypoint-initdb.d/20create_table_for_music.sql
@@ -21,11 +21,13 @@ CREATE TABLE album_artist (
 );
 
 --- create table song
+--- songhash is a sha256 hash
 CREATE TABLE song (
     idSong serial PRIMARY KEY,
     strTitle varchar(512) NOT NULL UNIQUE,
     idAlbum INTEGER REFERENCES album (idAlbum) NULL,
-    idSongfile INTEGER REFERENCES album (idAlbum) NULL
+    songhash char(64) NULL UNIQUE,
+    CONSTRAINT sha256hash CHECK (char_length(songhash) = 64)
 );
 
 --- create table song_artist as cross reference between song and artist
@@ -35,21 +37,12 @@ CREATE TABLE song_artist (
     UNIQUE (idSong, idArtist)
 );
 
---- create table songfile
---- songhash is a sha256 hash
-CREATE TABLE songfile (
-    idSongfile serial PRIMARY KEY,
-    songhash char(64) NOT NULL UNIQUE,
-    CONSTRAINT sha256hash CHECK (char_length(songhash) = 64)
-);
-
 --- create view songview with array of artist for a song
 CREATE VIEW songview AS
-    SELECT song.idSong, song.strTitle, array_agg(artist.strArtist) AS arrArtist, album.idAlbum, album.strAlbum, songfile.songhash
+    SELECT song.idSong, song.strTitle, song.songhash, array_agg(artist.strArtist) AS arrArtist, album.idAlbum, album.strAlbum
     FROM song
     LEFT OUTER JOIN song_artist ON (song.idsong = song_artist.idsong)
     LEFT OUTER JOIN artist ON (artist.idArtist = song_artist.idArtist)
     LEFT OUTER JOIN album ON (album.idAlbum = song.idAlbum)
-    LEFT OUTER JOIN songfile ON (songfile.idSongfile = song.idSongfile)
-    GROUP BY song.idSong, song.strTitle, album.idAlbum, album.strAlbum, songfile.songhash
+    GROUP BY song.idSong, song.strTitle, song.songhash, album.idAlbum, album.strAlbum
 ;
