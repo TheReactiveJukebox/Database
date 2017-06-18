2e770782b08ada00019f4979f385795e972c9b7d
Martin Ness <m.ness@martin89.de>
2017-06-18 14:56:44 +0200

    [song] db table columm renamed

--- docker-entrypoint-initdb.d/20create_table_for_music.sql
+++ docker-entrypoint-initdb.d/20create_table_for_music.sql
@@ -21,13 +21,13 @@ CREATE TABLE album_artist (
 );
 
 --- create table song
---- songhash is a sha256 hash
+--- hashSong is a sha256 hash
 CREATE TABLE song (
     idSong serial PRIMARY KEY,
     strTitle varchar(512) NOT NULL UNIQUE,
     idAlbum INTEGER REFERENCES album (idAlbum) NULL,
-    songhash char(64) NULL UNIQUE,
-    CONSTRAINT sha256hash CHECK (char_length(songhash) = 64)
+    hashSong char(64) NULL UNIQUE,
+    CONSTRAINT sha256hash CHECK (char_length(hashSong) = 64)
 );
 
 --- create table song_artist as cross reference between song and artist
@@ -40,10 +40,10 @@ CREATE TABLE song_artist (
 
 --- create view songview with array of artist for a song
 CREATE VIEW songview AS
-    SELECT song.idSong, song.strTitle, song.songhash, array_agg(artist.strArtist) AS arrArtist, album.idAlbum, album.strAlbum
+    SELECT song.idSong, song.strTitle, song.hashSong, array_agg(artist.strArtist) AS arrArtist, album.idAlbum, album.strAlbum
     FROM song
     LEFT OUTER JOIN song_artist ON (song.idsong = song_artist.idsong)
     LEFT OUTER JOIN artist ON (artist.idArtist = song_artist.idArtist)
     LEFT OUTER JOIN album ON (album.idAlbum = song.idAlbum)
-    GROUP BY song.idSong, song.strTitle, song.songhash, album.idAlbum, album.strAlbum
+    GROUP BY song.idSong, song.strTitle, song.hashSong, album.idAlbum, album.strAlbum
 ;
