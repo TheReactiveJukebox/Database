6d4dde8413804a9f237e212f65205641e3b37caf
Martin Ness <m.ness@martin89.de>
2017-06-23 18:17:38 +0200

    [Table song, album] Add columms for song duration and album cover

--- docker-entrypoint-initdb.d/20create_table_for_music.sql
+++ docker-entrypoint-initdb.d/20create_table_for_music.sql
@@ -14,6 +14,7 @@ CREATE TABLE album (
     idAlbum serial PRIMARY KEY,
     strAlbumNormalized varchar(256) NOT NULL UNIQUE,
     strAlbum text NOT NULL,
+    strCover text NULL,
     CONSTRAINT non_empty CHECK (length(strAlbumNormalized) > 0 and length(strAlbum) > 0)
 );
 
@@ -32,8 +33,10 @@ CREATE TABLE song (
     strTitle text NOT NULL,
     idAlbum INTEGER REFERENCES album (idAlbum) NULL,
     hashSong char(64) NULL UNIQUE,
+    iDuration integer NOT NULL,
     CONSTRAINT non_empty CHECK (length(strTitleNormalized) > 0 and length(strTitle) > 0),
-    CONSTRAINT sha256hash CHECK (char_length(hashSong) = 64)
+    CONSTRAINT sha256hash CHECK (char_length(hashSong) = 64),
+    CONSTRAINT nonnegativ_duration CHECK (iDuration >= 0)
 );
 
 --- create table song_artist as cross reference between song and artist
@@ -46,10 +49,10 @@ CREATE TABLE song_artist (
 
 --- create view songview with array of artist for a song
 CREATE VIEW songview AS
-    SELECT song.idSong, song.strTitle, song.hashSong, array_agg(artist.strArtist) AS arrArtist, album.idAlbum, album.strAlbum
+    SELECT song.idSong, song.strTitle, song.iDuration, song.hashSong, array_agg(artist.strArtist) AS arrArtist, album.idAlbum, album.strAlbum, album.strCover
     FROM song
     LEFT OUTER JOIN song_artist ON (song.idsong = song_artist.idsong)
     LEFT OUTER JOIN artist ON (artist.idArtist = song_artist.idArtist)
     LEFT OUTER JOIN album ON (album.idAlbum = song.idAlbum)
-    GROUP BY song.idSong, song.strTitle, song.hashSong, album.idAlbum, album.strAlbum
+    GROUP BY song.idSong, song.strTitle, song.iDuration, song.hashSong, album.idAlbum, album.strAlbum, album.strCover
 ;
