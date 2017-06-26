7def0201e1d078da30ace6b7b22f05e9b17bd80e
Martin Ness <m.ness@martin89.de>
2017-06-26 13:00:40 +0200

    [Columm names] Apply convention

--- docker-entrypoint-initdb.d/20create_table_for_music.sql
+++ docker-entrypoint-initdb.d/20create_table_for_music.sql
@@ -3,56 +3,56 @@
 
 --- create table artist
 CREATE TABLE artist (
-    idArtist serial PRIMARY KEY,
-    strArtistNormalized varchar(256) NOT NULL UNIQUE,
-    strArtist text NOT NULL,
-    CONSTRAINT non_empty CHECK (length(strArtistNormalized) > 0 and length(strArtist) > 0)
+    Id serial PRIMARY KEY,
+    NameNormalized varchar(256) NOT NULL UNIQUE,
+    Name text NOT NULL,
+    CONSTRAINT non_empty CHECK (length(NameNormalized) > 0 and length(Name) > 0)
 );
 
 --- create table album
 CREATE TABLE album (
-    idAlbum serial PRIMARY KEY,
-    strAlbumNormalized varchar(256) NOT NULL UNIQUE,
-    strAlbum text NOT NULL,
-    strCover text NULL,
-    CONSTRAINT non_empty CHECK (length(strAlbumNormalized) > 0 and length(strAlbum) > 0)
+    Id serial PRIMARY KEY,
+    TitelNormalized varchar(256) NOT NULL UNIQUE,
+    Titel text NOT NULL,
+    Cover text NULL,
+    CONSTRAINT non_empty CHECK (length(TitelNormalized) > 0 and length(Titel) > 0)
 );
 
 --- create table album_artist as cross reference between artist and album
 CREATE TABLE album_artist (
-    idArtist INTEGER REFERENCES artist (idArtist) NOT NULL,
-    idAlbum INTEGER REFERENCES album (idAlbum) NOT NULL,
-    UNIQUE (idArtist, idAlbum)
+    ArtistId INTEGER REFERENCES artist (id) NOT NULL,
+    AlbumId INTEGER REFERENCES album (id) NOT NULL,
+    UNIQUE (ArtistId, AlbumId)
 );
 
 --- create table song
 --- hashSong is a sha256 hash
 CREATE TABLE song (
-    idSong serial PRIMARY KEY,
-    strTitleNormalized varchar(512) NOT NULL UNIQUE,
-    strTitle text NOT NULL,
-    idAlbum INTEGER REFERENCES album (idAlbum) NULL,
-    hashSong char(64) NULL UNIQUE,
-    iDuration integer NOT NULL,
-    CONSTRAINT non_empty CHECK (length(strTitleNormalized) > 0 and length(strTitle) > 0),
-    CONSTRAINT sha256hash CHECK (char_length(hashSong) = 64),
-    CONSTRAINT nonnegativ_duration CHECK (iDuration >= 0)
+    Id serial PRIMARY KEY,
+    TitleNormalized varchar(512) NOT NULL UNIQUE,
+    Title text NOT NULL,
+    AlbumId INTEGER REFERENCES album (id) NULL,
+    Hash char(64) NULL UNIQUE,
+    Duration integer NOT NULL,
+    CONSTRAINT non_empty CHECK (length(TitleNormalized) > 0 and length(Title) > 0),
+    CONSTRAINT sha256hash CHECK (char_length(Hash) = 64),
+    CONSTRAINT nonnegativ_duration CHECK (Duration >= 0)
 );
 
 --- create table song_artist as cross reference between song and artist
 CREATE TABLE song_artist (
-    idSong INTEGER REFERENCES song (idSong) NOT NULL,
-    idArtist INTEGER REFERENCES artist (idArtist) NOT NULL,
-    boolFeatured boolean DEFAULT FALSE NOT NULL,
-    UNIQUE (idSong, idArtist)
+    SongId INTEGER REFERENCES song (id) NOT NULL,
+    ArtistId INTEGER REFERENCES artist (id) NOT NULL,
+    Featured boolean DEFAULT FALSE NOT NULL,
+    UNIQUE (SongId, ArtistId)
 );
 
 --- create view songview with array of artist for a song
 CREATE VIEW songview AS
-    SELECT song.idSong, song.strTitle, song.iDuration, song.hashSong, array_agg(artist.strArtist) AS arrArtist, album.idAlbum, album.strAlbum, album.strCover
+	SELECT song.Id AS SongId, song.Title, song.Duration, song.Hash, array_agg(artist.Name) AS Artists, album.Id AS AlbumId, album.Titel, album.Cover
     FROM song
-    LEFT OUTER JOIN song_artist ON (song.idsong = song_artist.idsong)
-    LEFT OUTER JOIN artist ON (artist.idArtist = song_artist.idArtist)
-    LEFT OUTER JOIN album ON (album.idAlbum = song.idAlbum)
-    GROUP BY song.idSong, song.strTitle, song.iDuration, song.hashSong, album.idAlbum, album.strAlbum, album.strCover
+    LEFT OUTER JOIN song_artist ON (song.id = song_artist.SongId)
+    LEFT OUTER JOIN artist ON (artist.id = song_artist.ArtistId)
+    LEFT OUTER JOIN album ON (album.id = song.AlbumId)
+    GROUP BY song.Id, song.Title, song.Duration, song.Hash, album.Id, album.Titel, album.Cover
 ;
