eeaff5c1633d904175cea7713beb60117098c1fd
Martin Ness <m.ness@martin89.de>
2017-07-13 10:06:09 +0200

    [Radio] Add new columm for Algorithm name

--- docker-entrypoint-initdb.d/21create_table_radio.sql
+++ docker-entrypoint-initdb.d/21create_table_radio.sql
@@ -5,5 +5,7 @@
 CREATE TABLE radio (
     Id serial PRIMARY KEY,
     UserId INTEGER REFERENCES jukebox_user (Id) NOT NULL,
-    IsRandom BOOLEAN NOT NULL
+    IsRandom BOOLEAN NOT NULL,
+    AlgorithmName varchar(16) NOT NULL,
+    CONSTRAINT non_empty CHECK (length(AlgorithmName) > 0)
 );
