3c8abef95d8008b3e44f6b25008e6dc700544f31
molwam <ben.wilkes@tu-dortmund.de>
2017-07-13 19:44:39 +0200

    [Feedback table] Changed table layout for feedback table

--- docker-entrypoint-initdb.d/23create_table_feedback.sql
+++ docker-entrypoint-initdb.d/23create_table_feedback.sql
@@ -2,30 +2,24 @@
 \connect reactivejukebox
 
 --- create table feedback
+--- When a user likes a certain feature of a song, the according column will be 1.  
+--- Likewise it will be -1, in case the user disliked a feature. 
+--- If the useres state of mind towards a feature of a song is unknown, the column will be 0.
+
 CREATE TABLE feedback (
     Id serial PRIMARY KEY,
     UserId INTEGER REFERENCES jukebox_user (Id) NOT NULL,
     SongId INTEGER REFERENCES song (Id) NOT NULL,
-    IsLike BOOLEAN NOT NULL
-);
-
---- create table feedbackreason
-CREATE TABLE feedbackreason (
-    Id serial PRIMARY KEY,
-    Description text NOT NULL,
-    UNIQUE (Description)
-);
-
---- create table feedback_radiostation
-CREATE TABLE feedback_radiostation (
-    FeedbackId INTEGER REFERENCES feedback (Id) NOT NULL,
     RadioId INTEGER REFERENCES radio (Id) NOT NULL,
-    UNIQUE (FeedbackId, RadioId)
+    FeedbackSong INTEGER NOT NULL,
+    FeedbackArtist INTEGER NOT NULL,
+    FeedbackTempo INTEGER NOT NULL,
+    FeedbackGenre INTEGER NOT NULL,
+    FeedbackDynamics INTEGER NOT NULL,
+    FeedbackPeriod INTEGER NOT NULL,
+    FeedbackMood INTEGER NOT NULL,
+    Time TIMESTAMP (0) without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
+    UNIQUE (SongId, RadioId, UserId)
 );
 
---- create table feedback_feedbackreason
-CREATE TABLE feedback_feedbackreason (
-    FeedbackId INTEGER REFERENCES feedback (Id) NOT NULL,
-    ReasonId INTEGER REFERENCES feedbackreason (Id) NOT NULL,
-    UNIQUE (FeedbackId, ReasonId)
-);
+
