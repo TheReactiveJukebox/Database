684276ff1032e8a53e99226d1b5dea2c735b0d6a
Martin Ness <m.ness@martin89.de>
2017-07-14 17:18:57 +0200

    [Radio] Add new columm for ReferenceSong

--- docker-entrypoint-initdb.d/21create_table_radio.sql
+++ docker-entrypoint-initdb.d/21create_table_radio.sql
@@ -7,5 +7,7 @@ CREATE TABLE radio (
     UserId INTEGER REFERENCES jukebox_user (Id) NOT NULL,
     IsRandom BOOLEAN NOT NULL,
     AlgorithmName varchar(16) NOT NULL,
-    CONSTRAINT non_empty CHECK (length(AlgorithmName) > 0)
+    ReferenceSongId INTEGER REFERENCES song (Id) NULL,
+    CONSTRAINT non_empty CHECK (length(AlgorithmName) > 0),
+    CONSTRAINT force_option CHECK (IsRandom or ReferenceSong IS NOT NULL)
 );
