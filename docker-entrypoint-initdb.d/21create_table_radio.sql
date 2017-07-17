426a2dc3b56dd895c1f602c6baddd6e8dc4cf629
Martin Ness <m.ness@martin89.de>
2017-07-17 12:03:51 +0200

    [Radio] ReferenceSong in seperate table

--- docker-entrypoint-initdb.d/21create_table_radio.sql
+++ docker-entrypoint-initdb.d/21create_table_radio.sql
@@ -6,6 +6,12 @@ CREATE TABLE radio (
     Id serial PRIMARY KEY,
     UserId INTEGER REFERENCES jukebox_user (Id) NOT NULL,
     AlgorithmName varchar(16) NOT NULL,
-    ReferenceSongId INTEGER REFERENCES song (Id) NULL,
     CONSTRAINT non_empty CHECK (length(AlgorithmName) > 0)
+);
+
+CREATE TABLE radio_song (
+    RadioId INTEGER REFERENCES radio (Id) NOT NULL,
+    SongId INTEGER REFERENCES song (Id) NOT NULL,
+    Position INTEGER NULL,
+    UNIQUE (RadioId, SongId)
 );
