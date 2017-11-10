caff2db325c3a62efdbc09fb1e866ab728ad84cd
Andreas Lang <andiwg@arcor.de>
2017-11-10 19:09:08 +0100

    [Feedback] remove Period feedback

--- docker-entrypoint-initdb.d/23create_table_feedback.sql
+++ docker-entrypoint-initdb.d/23create_table_feedback.sql
@@ -16,10 +16,9 @@ CREATE TABLE feedback (
     FeedbackSpeed INTEGER NOT NULL CHECK (FeedbackSpeed >= -1 AND FeedbackSpeed <= 1),
     FeedbackGenre INTEGER NOT NULL CHECK (FeedbackGenre >= -1 AND FeedbackGenre <= 1),
     FeedbackDynamics INTEGER NOT NULL CHECK (FeedbackDynamics >= -1 AND FeedbackDynamics <= 1),
-    FeedbackPeriod INTEGER NOT NULL CHECK (FeedbackPeriod >= -1 AND FeedbackPeriod <= 1),
     FeedbackMood INTEGER NOT NULL CHECK (FeedbackMood >= -1 AND FeedbackMood <= 1),
     Time TIMESTAMP (0) without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
-    UNIQUE (SongId, RadioId, UserId)
+    UNIQUE (SongId, UserId)
 );
 
 
