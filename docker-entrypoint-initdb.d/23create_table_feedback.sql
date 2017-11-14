04b5a71b3c3945e960f4020164d309ec0b4d1471
Andreas Lang <andiwg@arcor.de>
2017-11-14 13:03:27 +0100

    [Feedback] Remove Radio Id

--- docker-entrypoint-initdb.d/23create_table_feedback.sql
+++ docker-entrypoint-initdb.d/23create_table_feedback.sql
@@ -10,7 +10,6 @@ CREATE TABLE feedback (
     Id serial PRIMARY KEY,
     UserId INTEGER REFERENCES jukebox_user (Id) NOT NULL,
     SongId INTEGER REFERENCES song (Id) NOT NULL,
-    RadioId INTEGER REFERENCES radio (Id) NOT NULL,
     FeedbackSong INTEGER NOT NULL CHECK (FeedbackSong >= -1 AND FeedbackSong <= 1),
     FeedbackSpeed INTEGER NOT NULL CHECK (FeedbackSpeed >= -1 AND FeedbackSpeed <= 1),
     FeedbackDynamics INTEGER NOT NULL CHECK (FeedbackDynamics >= -1 AND FeedbackDynamics <= 1),
