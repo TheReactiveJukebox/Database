e3a5546aee30c4d3fcd9b0101e3f32935fc327b3
Sarahnad <sarah.graf@tu-dortmund.de>
2017-12-17 13:50:38 +0100

    [track_distance] Add reference and change int to integer

--- docker-entrypoint-initdb.d/26create_recomender_table.sql
+++ docker-entrypoint-initdb.d/26create_recomender_table.sql
@@ -4,8 +4,8 @@
 --- create table for absolute frequency of songs in playlists
 CREATE TABLE feature_distance
 (
-    track_from INT NOT NULL,
-    track_to INT NOT NULL,
+    track_from INTEGER REFERENCES song (Id) NOT NULL,
+    track_to INTEGER REFERENCES song (Id) NOT NULL,
     distance DOUBLE PRECISION NOT NULL,
     CONSTRAINT feature_distance_pk PRIMARY KEY (track_from, track_to)
 );
