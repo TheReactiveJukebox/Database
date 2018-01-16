22740f1f3724e78678a1a002d826e8eaead18fa2
Andreas Lang <andiwg@arcor.de>
2018-01-16 02:24:45 +0100

    [Feedback] change name for speed and mood

--- docker-entrypoint-initdb.d/23create_table_feedback.sql
+++ docker-entrypoint-initdb.d/23create_table_feedback.sql
@@ -45,17 +45,17 @@ CREATE TABLE feedbackAlbum(
 CREATE TABLE feedbackSpeed(
     Id serial PRIMARY KEY,
     UserId INTEGER REFERENCES jukebox_user (Id) NOT NULL,
-    Speed INTEGER NOT NULL,
+    fSpeed INTEGER NOT NULL,
     FeedbackSpeed INTEGER NOT NULL CHECK (FeedbackSpeed >= -1 AND FeedbackSpeed <= 1),
     Time TIMESTAMP (0) without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
-    UNIQUE (Speed, UserId)
+    UNIQUE (fSpeed, UserId)
 );
 
 CREATE TABLE feedbackMood(
     Id serial PRIMARY KEY,
     UserId INTEGER REFERENCES jukebox_user (Id) NOT NULL,
-    Mood INTEGER NOT NULL,
+    fMood INTEGER NOT NULL,
     FeedbackMood INTEGER NOT NULL CHECK (FeedbackMood >= -1 AND FeedbackMood <= 1),
     Time TIMESTAMP (0) without time zone NOT NULL DEFAULT (now() at time zone 'utc'),
-    UNIQUE (Mood, UserId)
+    UNIQUE (fMood, UserId)
 );
